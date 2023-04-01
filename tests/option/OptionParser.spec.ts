e6bf917b277ec1904f0d6c490699a5772f27a64c
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-04-01 19:49:14 +0200

    test: improve option parser tests coverage

--- tests/option/OptionParser.spec.ts
+++ tests/option/OptionParser.spec.ts
@@ -2,62 +2,17 @@ import { Argument, Option } from "../../lib";
 import { OptionParser } from "../../lib/option/OptionParser";
 
 describe("OptionParser", () => {
-  describe("parseValue", () => {
-    it("should parse a string value", () => {
-      const option = new Option({
-        shortName: "-f",
-        longName: "--file",
-        argument: Argument.create({ type: "string" }),
-      });
+  let option: Option;
+  let args: string[];
+  let index: number;
 
-      expect(OptionParser.parseValue(option, "file.txt")).toEqual("file.txt");
-    });
-
-    it("should parse a number value", () => {
-      const option = new Option({
-        shortName: "-n",
-        longName: "--number",
-        argument: Argument.create({ type: "number" }),
-      });
-
-      expect(OptionParser.parseValue(option, "42")).toEqual(42);
-    });
-
-    it("should parse a boolean value", () => {
-      const option = new Option({
-        shortName: "-b",
-        longName: "--boolean",
-        argument: Argument.create({ type: "boolean" }),
-      });
-
-      expect(OptionParser.parseValue(option, "true")).toEqual(true);
-      expect(OptionParser.parseValue(option, "false")).toEqual(false);
-    });
-
-    it("should throw an error for unknown type", () => {
-      const option = new Option({
-        shortName: "-x",
-        longName: "--unknown",
-        argument: { type: "invalid" } as any,
-      });
-
-      expect(() => OptionParser.parseValue(option, "value")).toThrowError(
-        "Unknown type: invalid"
-      );
-    });
+  beforeEach(() => {
+    option = new Option({ shortName: "-f", longName: "--file" });
+    args = ["-f", "file.txt"];
+    index = 0;
   });
 
   describe("parse", () => {
-    let option: Option;
-    let args: string[];
-    let index: number;
-
-    beforeEach(() => {
-      option = new Option({ shortName: "-f", longName: "--file" });
-      args = ["-f", "file.txt"];
-      index = 0;
-    });
-
     it("should parse a boolean option", () => {
       option = new Option({ shortName: "-v", longName: "--verbose" });
       args = ["-v"];
@@ -96,6 +51,7 @@ describe("OptionParser", () => {
     it("should parse a boolean option", () => {
       option = new Option({
         shortName: "-b",
+
         longName: "--boolean",
         argument: Argument.create({ type: "boolean" }),
       });
@@ -116,6 +72,47 @@ describe("OptionParser", () => {
       expect(() => OptionParser.parse(option, args, index)).toThrowError(
         "Unknown type: invalid"
       );
+    });
+
+    it("should throw an error for missing argument", () => {
+      option = new Option({
+        shortName: "-f",
+        longName: "--file",
+        argument: Argument.create({ type: "string", required: true }),
+      });
+      args = ["-f"];
+
+      expect(() => OptionParser.parse(option, args, index)).toThrowError(
+        "Missing argument for option --file"
+      );
+    });
+  });
+
+  describe("parseValue", () => {
+    it("should return 0 for a number option with an invalid value and no default value", () => {
+      option = new Option({
+        shortName: "-n",
+        longName: "--number",
+        argument: Argument.create({ type: "number" }),
+      });
+      args = ["-n", "invalid-value"];
+
+      const result = OptionParser.parse(option, args, index);
+
+      expect(result).toEqual({ value: 0, index: 1 });
+    });
+
+    it("should return the default value for a number option with an invalid value and a default value", () => {
+      option = new Option({
+        shortName: "-n",
+        longName: "--number",
+        argument: Argument.create({ type: "number", defaultValue: 10 }),
+      });
+      args = ["-n", "invalid-value"];
+
+      const result = OptionParser.parse(option, args, index);
+
+      expect(result).toEqual({ value: 10, index: 1 });
     });
   });
 });
