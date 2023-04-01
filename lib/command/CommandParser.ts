8f58ffd2fc88e677e1252c4ce1a5fec1f18be13e
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-04-01 18:39:26 +0200

    style: remove console log

--- lib/command/CommandParser.ts
+++ lib/command/CommandParser.ts
@@ -12,7 +12,6 @@ export class CommandParser {
     while (i < args.length) {
       const arg = args[i];
       if (arg.startsWith("-")) {
-        console.log(arg);
         const option = command.findOption(arg);
         if (!option) {
           throw new Error(`Unknown option: ${arg}`);
@@ -34,18 +33,12 @@ export class CommandParser {
     command: Command,
     options: Record<string, any>
   ): void {
-    console.log(JSON.stringify(command, null, 2));
-    console.log(JSON.stringify(options, null, 2));
     const optionKeys = command.options.map((option) => option.getKey());
-    console.log(optionKeys);
     for (const key in options) {
-      console.log(key);
       if (!optionKeys.includes(key)) {
         throw new Error(`Unknown option: ${key}`);
       }
-      console.log(JSON.stringify(command.options, null, 2));
       const option = command.findOption(key);
-      console.log(option);
       if (option?.argument?.required && options[key] === undefined) {
         throw new Error(`Missing required option: ${option.longName}`);
       }
